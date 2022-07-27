abdba0c00b4c5d3f8255157326700b57597e91f5
Nicolas GaliaM <nicolasgaliam@gmail.com>
2022-07-27 08:57:11 -0300

    terminamos de estructurar el ejercicio numero 1, solo falta ejecutarlo

---
+++ js/ejercicio1.js
@@ -0,0 +1,14 @@
+// 1- Crea un objeto llamado auto que tenga algunas características como el color, marca, modelo y si está encendido o apagado. Crea los métodos necesarios para permitir encender y apagar el auto.
+
+
+let auto = {
+color: "Azul",
+Marca: "Renault",
+Modelo: 12,
+Estado: "encendido",
+
+
+encender: ()=> {document.write ("<p>encendiendo el auto... </p>")},
+apagar: function (){document.write("<p>apagando el auto ... </p>")},
+}
+
